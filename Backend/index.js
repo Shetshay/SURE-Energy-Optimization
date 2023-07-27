460258fca2d5bb2464d3813da90ebcdda4c62432
Andrew <admin@stg-edu-136-168-91-155.dyn.csub.edu>
2023-07-27 12:07:41 -0700

    add database schema

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,14 @@ const axios = require('axios');
 
 const app = express();
 const PORT = 3000;
-const API_KEY = '9c588b8e394a9797f8013e82ead2ca35';
+const WEATHER_API_KEY = '9c588b8e394a9797f8013e82ead2ca35';
+const CALIFORNIAISO_API_KEY = 'YOUR_CALIFORNIAISO_API_KEY';
 
 app.get('/temperature', async (req, res) => {
   try {
     const city = 'Bakersfield';
     const state = 'California';
-    const weatherAPIUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=metric&appid=${API_KEY}`;
+    const weatherAPIUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=metric&appid=${WEATHER_API_KEY}`;
 
     const response = await axios.get(weatherAPIUrl);
     const temperature = response.data.main.temp;
@@ -21,7 +22,25 @@ app.get('/temperature', async (req, res) => {
   }
 });
 
+app.get('/energy-cost', async (req, res) => {
+  try {
+    const currentDate = new Date().toISOString().slice(0, 10);
+
+    const response = await axios.get(`https://oasisapi.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=${currentDate}T00:00-0000&enddatetime=${currentDate}T23:59-0000&version=1&market_run_id=RTPD&node=ALL&resultformat=6&queryformat=JSON`, {
+      headers: {
+        'Authorization': `Bearer ${CALIFORNIAISO_API_KEY}`,
+      },
+    });
+
+    const energyCostData = response.data;
+
+    res.json(energyCostData);
+  } catch (error) {
+    console.error('Error fetching energy cost data:', error.message);
+    res.status(500).json({ error: 'Failed to fetch energy cost data' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
